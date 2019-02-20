e5c5bc491646960cded8b3bf073c0c7592ce87aa
wisehackermonkey <oranbusiness@gmail.com>
2019-02-19 19:33:52 -0800

    added .idea to ignore file

--- init.rb
+++ init.rb
@@ -5,3 +5,19 @@
 # 20190219
 
 
+require 'optparse'
+
+# This will hold the options we parse
+options = {}
+
+OptionParser.new do |parser|
+
+  # Whenever we see -n or --name, with an
+  # argument, save the argument.
+  parser.on("-n", "--name NAME", "The name of the person to greet.") do |v|
+    options[:name] = v
+  end
+end.parse!
+
+# Now we can use the options hash however we like.
+puts "Hello #{ options[:name] }" if options[:name]
